41d2825de63e987ca80c18c59616b07801b72f86
Dub Eror <115782805+duberor@users.noreply.github.com>
2025-04-09 18:58:55 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let options = {
+            animate: true,
+            patternWidth: 90,
+            patternHeight: 90,
+            grainOpacity: 0.18,
+            grainDensity: .75,
+            grainWidth: .9,
+            grainHeight: .5
+};
+    
+grained('#hero', options);
